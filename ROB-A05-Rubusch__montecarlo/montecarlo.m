e402b278d78c799b43e5052ea0ecbeaac6403c60
Lothar Rubusch <l.rubusch@gmx.ch>
2013-03-29 17:52:09 +0100

    bayes fixed

--- ROB-A05-Rubusch__montecarlo/montecarlo.m
+++ ROB-A05-Rubusch__montecarlo/montecarlo.m
@@ -27,73 +27,73 @@ function [sense] = sensing( robot, worldmap )
 endfunction
 
 
-% discrete bayes localization
-function [posterior_worldmap] = bayeslocalization( worldmap, P_prior, sees_landmark, moves )
-  % init
-  worldsize = length( worldmap );
-  P_sensor = zeros( 1, worldsize );
-  P_motion = zeros( 1, worldsize );
-  prediction = zeros(1,worldsize);
+%% discrete bayes localization
+%function [posterior_worldmap] = bayeslocalization( worldmap, P_prior, sees_landmark, moves )
+%  % init
+%  worldsize = length( worldmap );
+%  P_sensor = zeros( 1, worldsize );
+%  P_motion = zeros( 1, worldsize );
+%  prediction = zeros(1,worldsize);
+%
+%  % probabilities, map
+%  p_landmark = sum( worldmap ) / worldsize;
+%  p_nolandmark = 1 - p_landmark;
+%
+%  % probabilities, sensor
+%  p_see = 0.8;                % P( see landmark       | landmark )
+%  p_see_err = 1-p_see;        % P( don't see landmark | landmark )
+%  p_notsee_err = 0.4;         % P( see landmark       | no landmark )
+%  p_notsee = 1-p_notsee_err;  % P( don't see landmark | no landmark )
+%
+%  for k=1:worldsize
+%    prediction(k) = 0;
+%
+%    %% motion model
+%    k_before = move( -moves, k, worldsize );
+%    for i=1:worldsize
+%      i_before = move( -moves, i, worldsize );
+%%      prediction(k) += P_prior( k ) * P_prior( k_before ) * P_prior( i_before );  % TODO ???
+%      prediction(k) += P_prior( k_before ) * P_prior( i_before );
+%    end
+%
+%    %% sensor model
+%    if 1 == worldmap(k)
+%      % map shows a landmark, and...
+%      if 1 == sees_landmark
+%        % ...a landmark sensed
+%        P_sensor(k) = p_see;% * p_landmark;
+%      else
+%        % ...no landmark sensed (incorrect)
+%        P_sensor(k) = p_see_err;% * p_landmark;
+%      end
+%    else
+%      % map has NO landmark, and...
+%      if 1 != sees_landmark
+%        % ...no landmark sensed
+%        P_sensor(k) = p_notsee;% * p_nolandmark;
+%      else
+%        % ...a landmark sensed (incorrect)
+%        P_sensor(k) = p_notsee_err;% * p_nolandmark;
+%      end
+%    end
+%    prediction(k) = P_sensor(k) * prediction(k);
+%  end
+%
+%  % normalization
+%  nu = 0;
+%  for k = 1:worldsize
+%    nu += prediction( k );
+%  end
+%  nu = 1/nu;
+%
+%  % formula
+%  for k = 1:worldsize
+%    posterior_worldmap(k) = nu * prediction(k);
+%  end
+%endfunction
 
-  % probabilities, map
-  p_landmark = sum( worldmap ) / worldsize;
-  p_nolandmark = 1 - p_landmark;
 
-  % probabilities, sensor
-  p_see = 0.8;                % P( see landmark       | landmark )
-  p_see_err = 1-p_see;        % P( don't see landmark | landmark )
-  p_notsee_err = 0.4;         % P( see landmark       | no landmark )
-  p_notsee = 1-p_notsee_err;  % P( don't see landmark | no landmark )
-
-  for k=1:worldsize
-    prediction(k) = 0;
-
-    %% motion model
-    k_before = move( -moves, k, worldsize );
-    for i=1:worldsize
-      i_before = move( -moves, i, worldsize );
-%      prediction(k) += P_prior( k ) * P_prior( k_before ) * P_prior( i_before );  % TODO ???
-      prediction(k) += P_prior( k_before ) * P_prior( i_before );
-    end
-
-    %% sensor model
-    if 1 == worldmap(k)
-      % map shows a landmark, and...
-      if 1 == sees_landmark
-        % ...a landmark sensed
-        P_sensor(k) = p_see;% * p_landmark;
-      else
-        % ...no landmark sensed (incorrect)
-        P_sensor(k) = p_see_err;% * p_landmark;
-      end
-    else
-      % map has NO landmark, and...
-      if 1 != sees_landmark
-        % ...no landmark sensed
-        P_sensor(k) = p_notsee;% * p_nolandmark;
-      else
-        % ...a landmark sensed (incorrect)
-        P_sensor(k) = p_notsee_err;% * p_nolandmark;
-      end
-    end
-    prediction(k) = P_sensor(k) * prediction(k);
-  end
-
-  % normalization
-  nu = 0;
-  for k = 1:worldsize
-    nu += prediction( k );
-  end
-  nu = 1/nu;
-
-  % formula
-  for k = 1:worldsize
-    posterior_worldmap(k) = nu * prediction(k);
-  end
-endfunction
-
-
-function particle_filter()
+function particlefilter()
 % for i=1; i<k do % normally done online
 %     for j=1; j<N do % for all particles
 %         compute a new state x by sampling according to P(x|u(i-1,x(j)));
@@ -142,10 +142,8 @@ moves = 0;
 printf("########################################################################\n");
 printf("DEBUG: robot on position %d\n", robot);
 
-
 % print world
 [ 0:9; worldmap ]
-
 
 
 % 1st iteration
