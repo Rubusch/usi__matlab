f936df40d9821a7a4c4477e066a9c34956ac053f
Lothar Rubusch <l.rubusch@gmx.ch>
2013-05-01 21:46:55 +0200

    3D space

--- ROB-A07-Rubusch__potential-field-navigation/potentialfieldnavigation.m
+++ ROB-A07-Rubusch__potential-field-navigation/potentialfieldnavigation.m
@@ -1,7 +1,7 @@
 % ASSIGNMENT07
 % Lothar Rubusch
 % 2013-Apr-29
-printf( "\n\nASSIGNMENT07 - Robotics\n" );
+printf( "ASSIGNMENT07 - Robotics\n" );
 printf( "Lothar Rubusch\n" );
 printf( "2013-Apr-29\n\n" );
 
@@ -113,83 +113,167 @@ clear; clc; close all;
 % -> attractive potential field
 % -> visualize in 2D and/or in 3D using MATLAB
 
-function db_print( M, YMAX, XMAX )
-  for yval=1:YMAX
-    printf( "\n" );
-    for xval=1:XMAX
-      val = M(XMAX * (yval-1) + xval , 3);
-      printf( "%2.1f\t", val );
+                                                                                
+%% DBUGGING
+
+function db_print( M )
+  num2str( M, "%.2f\t" )
+endfunction
+
+                                                                                
+
+%% return the euclidian distance between 2 points
+%% src - one distinct point: [y x]
+%% dst - coords of obstacle / destination:
+%% [ y1 x1;
+%%   y2 x2;
+%%   y3 x3 ]
+%%
+%% returns 0 if point is inside the area of dst
+function [ dist ] = distance( src, dst )
+  dists = [];
+  for i=1:size( dst )(1)
+    dx=0; dy=0;
+    dy = src(1) - dst(i,1);
+    dx = src(2) - dst(i,2);
+    dists(i) = sqrt( dy^2 + dx^2 );
+  endfor
+  dist = min( dists );
+endfunction
+
+%% function to normalize values between 0.0 and 1.0
+function [ M_normalized ] = normalize( M_raw )
+  M_normalized = 1/max(max( M_raw )) * M_raw;
+endfunction
+
+%% place an obstacle
+function [ M_ng ] = set_obstacle( M, obs )
+  k_rep = 5;
+  rho_zero = 10;
+  U_rep = 0; U_obs = 0;
+  for yval=1:size(M)(1)
+    for xval=1:size(M)(2)
+      dobs = distance( [yval xval], obs);
+
+      if ( 0 == dobs )
+        U_rep = k_rep;
+      else
+        U_rep = M(yval,xval) + 1/2 * k_rep * ( 1/dobs - 1/rho_zero )^2;
+        if k_rep < U_rep
+          U_rep = k_rep;
+        endif
+      endif
+      M_ng(yval,xval) = U_rep;
     endfor
   endfor
 endfunction
+
+function [ solution ] = solve( M, start )
+  % init
+  y = start(1);
+  x = start(2);
+  solution = [ y x ];
+  idx = 1;
+
+  while 1
+    y = solution(idx,1);
+    x = solution(idx,2);
+    idx += 1;
+
+    
+    break;
+
+  endwhile
+
+endfunction
+
+
+%% methods
+
+%% repulsive potential field
+function [ M_ng ] = repulsive_potential_field( M )
+  %% obstacle
+  obs1 = [ 26 44; 25 45 ; 26 45; 27 45; 26 46 ];
+  M = set_obstacle( M, obs1 );
+
+  obs2 = [ 16 4; 15 5 ; 16 5; 17 5; 16 6 ];
+  M = set_obstacle( M, obs2 );
+
+  obs3 = [ 36 4; 35 5 ; 36 5; 37 5; 36 6 ];
+  M = set_obstacle( M, obs3 );
+
+  obs4 = [ 6 14; 5 15 ; 6 15; 7 15; 6 16 ];
+  M = set_obstacle( M, obs4 );
+
+  obs5 = [ 16 24; 15 25 ; 16 25; 17 25; 16 26 ];
+  M = set_obstacle( M, obs5 );
+
+  obs6 = [ 16 54; 15 55 ; 16 55; 17 55; 16 56 ];
+  M = set_obstacle( M, obs6 );
+
+  obs7 = [ 36 54; 35 55 ; 36 55; 37 55; 36 56 ];
+  M = set_obstacle( M, obs7 );
+
+  obs8 = [ 10 43; 11 42 ; 11 43; 12 44; 11 45 ];
+  M = set_obstacle( M, obs8 );
+
+  obs9 = [ 26 22; 25 23 ; 26 23; 27 23; 26 24 ];
+  M = set_obstacle( M, obs9 );
+
+  M_ng = M;
+endfunction
+
+%% attractive potential field
+function [ M_ng ] = attractive_potential_field( M, goal )
+  k_att = 10;
+  U_att = 0;
+  for yval=1:size(M)(1)
+    for xval=1:size(M)(2)
+      %% base potential
+      rho_goal = distance( [yval xval], goal );
+
+      %% attractive
+      U_att = 1/2 * k_att * rho_goal^2;
+
+      %% potential field
+      U(yval, xval) = U_att;
+    endfor
+  endfor
+  U = normalize( U );
+  M_ng = M + U;
+endfunction
+
+
+%% START                                                                        
 
 
 %% set up field
 XMAX=60
 YMAX=40
-dx=0; dy=0;
-
-%% goal
-goal = [ 10 10 ];
-
-%% obstacle
-#obst1 = [ 5 5 ; 6 5; 7 5 ];
-obst1 = [ 5 10 ];  
-
-%% method
-k_att = 0.5;    
-
-M=[];
-for yval=1:YMAX
-  for xval=1:XMAX
-
-    %% base potential
-    dx = xval - goal(1);
-    dy = yval - goal(2);
-    pot = sqrt(dx^2 + dy^2);
-
-    M(xval,yval) = pot;
+M=zeros(YMAX, XMAX);
+goal = [ 40 60 ];
+start = [ 0 0 ];
+ty = [1:size(M)(1)];
+tx = [1:size(M)(2)];
+% [xx,yy] = meshgrid( ty, tx );
 
 
-    %% attraction
-%    pot = -k_att * pot;
+%% repulsive potential field
+M = repulsive_potential_field( M );
+M = normalize( M );
 
-    %% operations...
-    % TODO    
-
-    %% model obstacles
-% TODO
-%    for xobst=1:length(obst1)
-%      for yobst=1:length(obst1)
-%        dx = xval - xobst;
-%        dy = yval - yobst;
-%        
-%        pot += sqrt(dx^2 + dy^2);     
-%      endfor
-%    endfor
-%
-    %% round up
-%    M = [M ; [xval yval pot]];
-
-  endfor
-endfor
-
-%% DEBUGGING
-%db_print( M, YMAX, XMAX );
-%M
-#plot3(M(:,1), M(:,3), M(:,2), "b*")
-#plot3(M(:,3), M(:,1), M(:,2), "b.")
-%num2str( M, "%2.1f " )   
-%tx = ty = linspace(1,XMAX,XMAX);
+%% attractive potential field
+M = attractive_potential_field( M, goal );
+M = normalize( M );
 
 %% DEBUG
-tx = [1:XMAX];
-ty = [1:YMAX];
-[xx,yy] = meshgrid( tx, ty );
-mesh( tx, ty, M' );
+figure
+mesh( tx, ty, M );
+hold on
+solution = solve( M, start );
+plot3( solution, "r" );
 
-
-
+% TODO rm
 %mesh(M(:,1), M(:,3), M(:,2))
 
 %% find way through it
