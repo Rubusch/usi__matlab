216271f2ffdc38dd9d228272d2228a599c334e6b
Lothar Rubusch <l.rubusch@gmx.ch>
2013-05-07 14:50:48 +0200

    preparations

--- ROB-A08-robot-arm-kinematics/robotarmkinematics.m
+++ ROB-A08-robot-arm-kinematics/robotarmkinematics.m
@@ -9,3 +9,40 @@ printf( "2013-May-06\n\n" );
 clear; clc; close all;
 
 
+% function
+function robotarmkinematics( theta )
+  theta
+% TODO
+endfunction
+
+
+% a)
+printf( "a)\n" );
+theta = [ 0 ; 0 ; 0 ; 0 ; 0 ]
+robotarmkinematics( theta )
+
+% b)
+printf( "b)\n" );
+theta = [ 0    ; pi/4 ; -pi/4 ; -pi/4+pi/4)+pi ; 0 ];
+robotarmkinematics( theta );
+
+% c)
+printf( "c)\n" );
+theta = [ pi/4 ; pi/4 ; -pi/4 ; -pi/4+pi/4+pi ; 0 ];
+robotarmkinematics( theta );
+
+% d)
+printf( "d)\n" );
+theta = [ 0    ; pi/2 ; -pi/2 ; -pi/2+pi/2+pi ; 0 ];
+robotarmkinematics( theta );
+
+% e)
+printf( "d)\n" );
+theta = [ pi/4 ; pi/2 ; -pi/2 ; -pi/2+pi/2+pi ; 0 ];
+robotarmkinematics( theta );
+
+
+
+printf( "READY.\n" );
+
+
